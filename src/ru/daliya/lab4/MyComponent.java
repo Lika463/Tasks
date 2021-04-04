db3a99dff2b414a96f9ba8849d1977df5977af67
Daliya <vakilova.daliya@bk.ru>
2021-04-04 11:58:31 +0300

    Код кнопки, которая не работает

--- src/ru/daliya/lab4/MyComponent.java
+++ src/ru/daliya/lab4/MyComponent.java
@@ -4,21 +4,30 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyComponent extends JComponent {
-    @Override
-    protected void paintComponent(Graphics g) {
-        g.setColor(Color.CYAN);
-        g.fillRect(100, 100, 200, 200 );
+    private static int ScreenSize;
 
-
-    }
-
+    //    @Override
+//    protected void paintComponent(Graphics g) {
+//        g.setColor(Color.CYAN);
+//        g.fillRect(100, 100, 200, 200 );
+//    }
     public static void main(String[] args) {
-        JFrame frame=new JFrame("JComponent");
-        frame.setSize(600,600);
+        JFrame frame = new JFrame("JComponent");
+        frame.setSize(600, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
 
         MyComponent comp = new MyComponent();
         frame.getContentPane().add(comp);
     }
-}
+        public static void createAndShowGUI() {
+            //кнопка сброса
+            Button reset = new Button("Reset");
+            reset.setSize(ScreenSize, 50);
+//            reset.addActionListener(new JButtonClick());
+            reset.setVisible(true);
+
+        }
+    }
+
